5080031516fd90755227770e5b790e3e68226a47
liuzhou <liuzhou@shineyue.com>
2022-04-01 17:44:53 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './style.scss'
+
+class App extends Component {
+    render() {
+        return (
+            <div style={{ color: 'red' }} className='box'>Hello,React</div>
+        )
+    }
+}
+
+export default App
